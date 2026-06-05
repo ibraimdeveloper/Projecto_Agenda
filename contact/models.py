3f3f0db2296979e05858b8a4a0572232821207ce
ibraim <ibraimaugusto60@gmail.com>
2026-06-05 18:22:04 +0100

    ok

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+# first_name(string), last_name(string), email(string), phone_number(string)
+# created_date(date), description(text), category (foreign key), show(boolean)
+# owner(foreign key), picture(imagem)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from contact import views
 
 # Create your views here.
+
+def view(request):
+    return render(request, 'contact/index.html')

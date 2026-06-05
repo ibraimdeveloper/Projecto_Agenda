2c2a2538721b9a8b016716a17a0f875cd4e061e5
ibraim <ibraimaugusto60@gmail.com>
2026-06-05 19:27:57 +0100

    ok

--- contact/admin.py
+++ contact/admin.py
@@ -7,4 +7,9 @@ from contact import models
 @admin.register(models.Contact)
 
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'email', 'phone_number', 'created_date'
+    ordering = '-id',
+    filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name',
+    list_per_page = 10
